22890423393fb4030531bf16e691169e3856c4ea
inaridiy <inainainariinainari@gmail.com>
2021-08-24 22:29:25 +0900

    スマホも仮実装

--- src/components/app/MobileMenu.vue
+++ src/components/app/MobileMenu.vue
@@ -37,11 +37,11 @@
 
       <v-divider />
 
-      <v-list-item link>
+      <v-list-item link to="/post">
         <v-list-item-icon>
-          <v-icon>mdi-folder</v-icon>
+          <v-icon>mdi-fountain-pen-tip</v-icon>
         </v-list-item-icon>
-        <v-list-item-title>My Files</v-list-item-title>
+        <v-list-item-title>投稿管理</v-list-item-title>
       </v-list-item>
       <v-list-item link>
         <v-list-item-icon>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,1 +1,19 @@
-<template></template>
+<template>
+  <v-btn
+    fab
+    large
+    right
+    fixed
+    darl
+    color="primary"
+    :to="`/post/edit`"
+    class="plusBtn"
+  >
+    <v-icon> mdi-plus </v-icon>
+  </v-btn>
+</template>
+<style scoped>
+.plusBtn {
+  bottom: 80px;
+}
+</style>
